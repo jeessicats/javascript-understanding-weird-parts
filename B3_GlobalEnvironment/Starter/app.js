7fe4925b0ff7b2d5b0bc748d1ef416bc7f8e6640
jeessicats <jeessicats@gmail.com>
2025-07-11 15:37:24 -0300

    types and operators

---
+++ B3_GlobalEnvironment/Starter/app.js
@@ -0,0 +1,12 @@
+function b() {
+    console.log('This is function b');
+}
+
+b();
+
+console,log(a);// This code demonstrates the global environment in JavaScript
+
+let a = 'Hello, World!';
+
+console.log(a);
+
